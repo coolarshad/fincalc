63a5dd42372bc119f0ad6c5ab76e0544b8ecb857
saiyad <saiyad@fetch.ly>
2026-07-19 15:49:34 +0545

    final try for fix indexing issue

--- src/app/robots.tsx
+++ src/app/robots.tsx
@@ -8,7 +8,9 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/api/'], // Disallow API routes from being crawled
+      disallow: [
+        '/api/', // Disallow API routes from being crawled
+      ],
     },
     sitemap: `${baseUrl}/sitemap.xml`,
   };
